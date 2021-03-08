2797247a58224c349c385d4d29cd3e8a5484530a
brien17 <cbrien0661@eagle.fgcu.edu>
2021-03-07 20:00:14 -0500

    Added LoginTest class

---
+++ SWT-AirlineProduct/test/LoginTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.sql.Connection;
+
+public class LoginTest {
+    @Test
+    public void validLoginTest() {
+        Login login = new Login();
+        String username = "cbrien";
+        String password = "password";
+        Connection con = null;
+        String output = login.login(username, password, con);
+        assertEquals("valid", output);
+    }
+
+    @Test
+    public void invalidLoginTest() throws AWTException, InterruptedException {
+        Login login = new Login();
+        String username = "cbrien";
+        String password = "123";
+        Connection con = null;
+        String output = login.login(username, password, con);
+
+        assertEquals("invalid", output);
+    }
+
+    @Test
+    public void emptyLoginTest() {
+        Login login = new Login();
+        String username = "";
+        String password = "";
+        Connection con = null;
+        String output = login.login(username, password, con);
+
+        assertEquals("blank", output);
+    }
+}
