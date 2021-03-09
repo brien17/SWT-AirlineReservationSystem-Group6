5cd425f67c57bd952c83337d9256007938c7d627
brien17 <cbrien0661@eagle.fgcu.edu>
2021-03-09 09:47:34 -0500

    Fixed error from merge

--- SWT-AirlineProduct/src/TicketController.java
+++ SWT-AirlineProduct/src/TicketController.java
@@ -29,7 +29,7 @@ public class TicketController extends javax.swing.JInternalFrame {
      */
     public TicketController() {
         initComponents();
-        autoID();
+        autoIncrementTicketNo();
     }
 
     Connection con;
