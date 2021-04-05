2a7554228fb849c24e6f45f2f876882a955bced4
brien17 <cbrien0661@eagle.fgcu.edu>
2021-04-05 16:28:31 -0400

    Fixed Merge Conflict from merging master

--- SWT-AirlineProduct/src/Login.java
+++ SWT-AirlineProduct/src/Login.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
