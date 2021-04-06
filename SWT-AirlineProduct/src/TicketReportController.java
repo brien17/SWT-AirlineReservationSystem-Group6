28b90530312536b47dfc97057c10b778a2081357
codyb <cbuckley6232@eagle.fgcu.edu>
2021-04-05 21:17:53 -0400

    Fixing updates to Branch "Cody"

--- SWT-AirlineProduct/src/TicketReportController.java
+++ SWT-AirlineProduct/src/TicketReportController.java
@@ -17,12 +17,12 @@ import javax.swing.table.DefaultTableModel;
  */
 
 
-public class ticketreport extends javax.swing.JInternalFrame {
+public class TicketReportController extends javax.swing.JInternalFrame {
 
     /**
      * Creates new form ticketreport
      */
-    public ticketreport() {
+    public TicketReportController() {
         initComponents();
         LoadData();
     }
@@ -128,9 +128,9 @@ public class ticketreport extends javax.swing.JInternalFrame {
 
 
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ticket.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(TicketController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(ticket.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(TicketController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
 
