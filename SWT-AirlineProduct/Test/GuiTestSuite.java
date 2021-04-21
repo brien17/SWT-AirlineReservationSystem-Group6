942ff6f230bd8497c85e529b03c89b0a917c734c
brien17 <cbrien0661@eagle.fgcu.edu>
2021-04-20 22:55:52 -0400

    Renamed some classes to solve test suite issues

--- SWT-AirlineProduct/Test/GuiTestSuite.java
+++ SWT-AirlineProduct/Test/GuiTestSuite.java
@@ -2,8 +2,15 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
+/**
+ * This class serves as a test suite to run all of the GUI tests
+ */
+
 @RunWith(JUnitPlatform.class)
-@SelectClasses({MainGuiTest.class,TicketControllerGuiTest.class})
+@SelectClasses({
+        MainGuiTest.class,
+        TicketControllerGuiTest.class
+})
 public class GuiTestSuite {
 
 }
