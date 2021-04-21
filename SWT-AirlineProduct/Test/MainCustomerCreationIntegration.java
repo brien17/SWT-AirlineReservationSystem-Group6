ed009f8bb8b4c66934427baa5e97990abbc16541
Austin Nolz <nolzaj93@gmail.com>
2021-04-20 23:21:51 -0400

    Update MainCustomerCreationIntegration.java

--- SWT-AirlineProduct/Test/MainCustomerCreationIntegration.java
+++ SWT-AirlineProduct/Test/MainCustomerCreationIntegration.java
@@ -7,14 +7,14 @@ public class MainCustomerCreationIntegration {
    * Create instance of Main and stub
    */
 
-  /** Test Case ID: TC-INT-03
+  /** Test Case ID: TC-NF-01
    * Requirement ID/Description: SR-NF-09  The system shall allow users to open up
    *            at least 100 windows in the Main menu.
    * Purpose: Stress test the integration link between Main and lower level components.
    * Test Setup: An instance of the MainChildrenIntegration class is created with
    *            a composed class variable of type main set with a new object
    * Test Strategy: Top down integration testing with stub
-   * Input: MainChildrenIntegration{ Main main = new Main();  }, int view_count = 0
+   * Input: MainChildrenIntegration stressIntegrationTest(object), int view_count = 0
    * Expected Output: 100
    */
 
